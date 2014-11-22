29594ed22c69d5b8735293ea697d8a03cd45d849
Conor Brady <conorjbrady1@gmail.com>
2014-11-22 11:15:57 +0000

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talk2me::Application.config.secret_key_base = '0d5752f74527b10d0d7de9c00b87e66d478f48a3b59c1656e2cc67bac408c6c3922c8955b8f3e037ede18aaeffc6f18988c3b50ce1eb581606d0c0cc09662636'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Talk2me::Application.config.session_store :cookie_store, key: '_talk2me_session'
