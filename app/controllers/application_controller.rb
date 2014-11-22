e07ae399daeb65d51df30aa64b7c03ca68929b00
Conor Brady <conorjbrady1@gmail.com>
2014-11-22 15:51:07 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
 
   before_filter :authenticate
+  after_filter :cors_set_access_control_headers
+  skip_before_filter :authenticate, :only => [:route_options]
+
+  def route_options
+      cors_preflight_check
+  end
 
   private
 
@@ -13,6 +19,23 @@ class ApplicationController < ActionController::Base
           if user
               @current_user = user.authenticate(password)
           end
+      end
+  end
+
+  def cors_set_access_control_headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+      response.headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+      if request.method == 'OPTIONS'
+          request.headers['Access-Control-Allow-Origin'] = '*'
+          request.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+          request.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Auth-Token, Email'
+          request.headers['Access-Control-Max-Age'] = '1728000'
+          render :text => '', :content_type => 'text/plain'
       end
   end
 end
